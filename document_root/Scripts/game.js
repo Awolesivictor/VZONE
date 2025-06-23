3deefba96eb35322be54f8c3c972f21f32f9155c
Awolesivictor <164688202+Awolesivictor@users.noreply.github.com>
2025-06-23 16:18:49 +0100

    Add files via upload

---
+++ document_root/Scripts/game.js
@@ -0,0 +1,7 @@
+function showsidebar() {
+  document.getElementById("sidebar").style.width = "245px";
+  // document.getElementsByTagName('*').style.backgroundColor = "#02060e71"
+}
+function closesidebar() {
+  document.getElementById("sidebar").style.width = "0";
+}
